c9482c10d20ca98b12ac8b1bce5a16d08d08e359
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-18 19:05:07 +0600

    feat: initialize React entry point with router, providers, and error boundary wrappers

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,22 @@ import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { App } from "@/App";
 import { ThemeProvider } from "@/contexts/ThemeContext";
+import { InstituteDataProvider } from "@/contexts/InstituteDataContext";
+import { ErrorBoundary } from "@/components/common/ErrorBoundary";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <ThemeProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ErrorBoundary>
+      <HelmetProvider>
+        <ThemeProvider>
+          <BrowserRouter>
+            <InstituteDataProvider>
+              <App />
+            </InstituteDataProvider>
+          </BrowserRouter>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
